10e5323f5ceb6201c455fe7a0fb41229d52447fc
carrin808 <hezekielcarringtone@gmail.com>
2022-10-13 16:38:29 +0300

    Quiz App

---
+++ src/helpers/Questions.js
@@ -0,0 +1,34 @@
+export const Questions = [
+  {
+    prompt: "What is my name?",
+    optionA: "John",
+    optionB: "Jake",
+    optionC: "Josh",
+    optionD: "Carrington",
+    answer: "optionD",
+  },
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "MC-03",
+    optionD: "Java",
+    answer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    answer: "optionD",
+  },
+  {
+    prompt: "Where do I live?",
+    optionA: "New York",
+    optionB: "California",
+    optionC: "Texas",
+    optionD: "Nairobi",
+    answer: "optionD",
+  },
+];
